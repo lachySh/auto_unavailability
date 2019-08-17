532bb693e29dd154e3bcc12cc736d0db2427b980
Lachie Shewring <lachy.sh@gmail.com>
2019-08-17 22:53:38 +1000

    somethings broken and i dont know what

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QDialog, QVBoxLayout, QCalendarWidget, QLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QDialog, QVBoxLayout, QCalendarWidget, QLayout, QLineEdit,QGridLayout
 import sys
 from PyQt5 import QtGui, QtCore
-
-app = QApplication([])
-
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+from datetime import datetime  
+from datetime import timedelta
 class Window(QDialog):
     def __init__(self):
         super().__init__()
@@ -14,27 +14,100 @@ class Window(QDialog):
         self.height = 400
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.Calendar()
+
+        f = open("tdb.txt", "r")
+        startdate = f.readline()
+        starr = startdate.split(".")
+        print(starr)
+
+        #date = QDate.fromString()
+
+        vbox = QVBoxLayout()
+        calendar = QCalendarWidget()
+        calendar.setGridVisible(True)
+        calendar.setFirstDayOfWeek(QtCore.Qt.Thursday)
+        #calendar.setSelectedDate()
+        
+        lbl = QLabel()
+        lbl.setText("Select a Thursday date to start week with")
+        vbox.addWidget(lbl)
+
+        vbox.addWidget(calendar)
+
+        #self.setLayout(grid)
+        f = open("tdb.txt", "r")
+        startdate = f.readline()
+        enddate = f.readline()
+        times = []
+        temp_times = []
+
+        for n in range(0,7):
+            temp_times = f.readline().split()
+            times.append(temp_times)
+        print(times)
+        
+        grid = QGridLayout()
+
+        day = ["Thurs","Fri","Sat","Sun","Mon","Tues","Weds"]
+        for i in range(1,8):
+            daycount = QLabel()
+            daycount.setText(day[i-1])
+            grid.addWidget(daycount,i,0)
+            for j in range(2,6):
+                box = QLineEdit()
+                box.setText(times[i-1][j-2])
+                grid.addWidget(box,i,j)
+
+        bbox = QVBoxLayout()
+        button = QPushButton()
+        button.setText("Set Unavailability")
+        button.clicked.connect(handleButton)
+        bbox.addWidget(button)
+
+        
+
+        vall = QVBoxLayout()
+        vall.addLayout(vbox)
+        vall.addLayout(grid)
+        vall.addLayout(bbox)
+        self.setLayout(vall)
+
+        
 
         self.show()
 
-    def Calendar(self):
-        vbox = QVBoxLayout()
-        self.calendar = QCalendarWidget()
-        self.calendar.setGridVisible(True)
-        self.calendar.setFirstDayOfWeek(QtCore.Qt.Thursday)
-        date = self.calendar.selectedDate()
-        self.lbl = QLabel()
-        self.lbl.setText("Select a Thursday date to start week with")
-        vbox.addWidget(self.lbl)
-        print(date)
+        """def handleButton():
+            date = calendar.selectedDate()
+            strdate = QDate.toPyDate(date)
+            enddate = strdate + timedelta(days=6)
+            print(enddate)
+            strdate = str(strdate)
+            strdate = strdate.split("-")
+            enddate = str(enddate)
+            enddate = enddate.split("-")
+            print(strdate)
+            #FILE READING
+            f = open("tdb.txt", "r")
+            startdate = f.readline()
+            enddate = f.readline()
 
-        vbox.addWidget(self.calendar)
+            times = []
+            temp_times = []
+
+            for n in range(0,7):
+                temp_times = f.readline().split()
+                times.append(temp_times)
+                #print(temp_times)
+            msg = f.readline()
+            #FILE WRITING
+            f = open("tdb.txt", "w")
+            f.write(strdate[2]+"."+strdate[1]+"."+strdate[0])
+            f.write(enddate[2]+"."+enddate[1]+"."+enddate[0])"""
+
         
 
-        self.setLayout(vbox)
-
 App = QApplication(sys.argv)
+App.setStyle('Fusion')
 window = Window()
 sys.exit(App.exec())
 
